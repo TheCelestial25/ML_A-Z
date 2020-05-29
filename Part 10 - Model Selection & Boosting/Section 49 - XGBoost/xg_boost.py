d9937c551d941516200d8a6ebaf7c935f4476d3c
theCelestial25 <37958393+TheCelestial25@users.noreply.github.com>
2020-05-29 12:13:57 +0530

    Add all files.

---
+++ Part 10 - Model Selection & Boosting/Section 49 - XGBoost/xg_boost.py
@@ -0,0 +1,48 @@
+# XGBoost
+
+# Installing XGBoost
+# Enter the following command in a terminal (or anaconda prompt for Windows users): conda install -c conda-forge xgboost
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+# Male/Female
+labelencoder_X = LabelEncoder()
+X[:, 2] = labelencoder_X.fit_transform(X[:, 2])
+# Country column
+ct = ColumnTransformer([("Geography", OneHotEncoder(), [1])], remainder = 'passthrough')
+X = ct.fit_transform(X)
+X = X[:, 1:] # Dummy variable trap
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Fitting XGBoost to the Training set
+from xgboost import XGBClassifier
+classifier = XGBClassifier()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Applying k-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+accuracies.mean()
+accuracies.std()
